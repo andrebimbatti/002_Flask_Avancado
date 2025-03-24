64f831a73771a3ddc5f1d4b4c6afe9cf6428d469
Andre Bimbatti <andrebimbatti87@gmail.com>
2025-03-24 19:58:57 -0300

    corrigido bug editar e novo html + atualizado formulario login

--- helpers.py
+++ helpers.py
@@ -2,13 +2,18 @@ import os
 from jogoteca import app
 from markupsafe import Markup
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, SubmitField
+from wtforms import StringField, PasswordField, validators, SubmitField
 
 class FormularioJogo(FlaskForm):
     nome = StringField('Nome do Jogo', [validators.DataRequired(), validators.Length(min=1, max=50)])
     categoria = StringField('Categoria', [validators.DataRequired(), validators.Length(min=1, max=40)])
     console = StringField('Console', [validators.DataRequired(), validators.Length(min=1, max=20)])
     salvar = SubmitField('Salvar')
+
+class FormularioUsuario(FlaskForm):
+    nickname = StringField('Nickname', [validators.DataRequired(), validators.Length(min=1, max=8)])
+    senha = PasswordField('Senha', [validators.DataRequired(), validators.Length(min=1, max=100)])
+    login = SubmitField('Login')
 
 
 def recupera_imagem(id):
